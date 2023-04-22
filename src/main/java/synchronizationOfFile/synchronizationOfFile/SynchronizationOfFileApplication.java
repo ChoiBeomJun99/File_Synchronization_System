e81b7d790873fdbf5a0ed9ef6c54bed7a40206ff
최범준 <choebeomjun@choebeomjun-ui-MacBookAir.local>
2023-04-22 23:35:55 +0900

    first commit

---
+++ src/main/java/synchronizationOfFile/synchronizationOfFile/SynchronizationOfFileApplication.java
@@ -0,0 +1,13 @@
+package synchronizationOfFile.synchronizationOfFile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynchronizationOfFileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynchronizationOfFileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/synchronizationOfFile/synchronizationOfFile/SynchronizationOfFileApplicationTests.java
@@ -0,0 +1,13 @@
+package synchronizationOfFile.synchronizationOfFile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchronizationOfFileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
